9fc6fd22492ad1a8f7baa21c533c2e2b4c25e8c0
Aniket <aniket>
2018-06-17 12:23:05 +0530

    Code addition

---
+++ SampleApplication.IDP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleApplication.IDP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ SampleOpenIDDemo.API/App_Start/Startup.Auth.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Owin.Security.Cookies;
+using Owin;
+using Microsoft.Owin.Security;
+
+
+using Microsoft.Owin.Security.OpenIdConnect;
+
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Microsoft.IdentityModel.Tokens;
+
+
+namespace SampleOpenIDDemo.API
+{
+    public partial class Startup
+    {
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+
+
+            app.UseOpenIdConnectAuthentication(
+                new OpenIdConnectAuthenticationOptions
+                {
+                    
+                    ClientId = "AngularCore",
+                    Authority = "https://localhost:44381/",
+                    TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidAudience = "mywebapicore",
+                        ValidateAudience = true
+                    },
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        RedirectToIdentityProvider = (context) =>
+                        {
+                            Debug.WriteLine("*** RedirectToIdentityProvider");
+                            return Task.FromResult(0);
+                        },
+                        MessageReceived = (context) =>
+                        {
+                            Debug.WriteLine("*** MessageReceived");
+                            return Task.FromResult(0);
+                        },
+                        SecurityTokenReceived = (context) =>
+                        {
+                            Debug.WriteLine("*** SecurityTokenReceived");
+                            return Task.FromResult(0);
+                        },
+                        SecurityTokenValidated = (context) =>
+                        {
+                            Debug.WriteLine("*** SecurityTokenValidated");
+                            return Task.FromResult(0);
+                        },
+                        AuthorizationCodeReceived = (context) =>
+                        {
+                            Debug.WriteLine("*** AuthorizationCodeReceived");
+                            return Task.FromResult(0);
+                        },
+                        AuthenticationFailed = (context) =>
+                        {
+                            Debug.WriteLine("*** AuthenticationFailed");
+                            return Task.FromResult(0);
+                        }
+
+                    }
+                });
+        }
+    }
+}
